ec06ac3c100e8249d3f31e5778d14f44fd3619cb
Nick Rizzo <nickmike55@gmail.com>
2019-09-14 17:26:57 -0400

    Auto commit 17:26:57.52 Sat 09/14/2019

--- gitAutosave.bat
+++ gitAutosave.bat
@@ -2,6 +2,7 @@
 
 REM '%~dp0' returns the drive and path to this file
 SET minecraft_saves_repo=%~dp0
-cd %minecraft_saves_repo%;
+echo %minecraft_saves_repo%
+cd %minecraft_saves_repo%
 git add -A
 git commit -m "Auto commit %time% %date%"
